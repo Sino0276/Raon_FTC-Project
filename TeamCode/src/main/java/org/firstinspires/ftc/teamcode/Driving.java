069965e0e69a9fb64e4657743fe4beabb59ab81e
sineno <106243747+Sino0276@users.noreply.github.com>
2025-10-20 17:31:50 +0900

    addServoTest
    configurated servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driving.java
@@ -1,27 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.teamcode.configs.DriveConfig.*;
-
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
-import org.firstinspires.ftc.teamcode.configs.DriveConfig;
-
+@Config
 @TeleOp(name = "mtrTest", group = "java")
 public class Driving extends OpMode {
+    public static PIDFCoefficients DRIVE_PID = new PIDFCoefficients(5, 2, 1, 11);
+    /// max TPS == 2800
+    public static int TPS = 2800;
+    /// targetPosition 허용 오차
+    public static int TOLERANCE = 1;
+    /// 1 inch = 45 tick
+    public static int TARGET_POS = 45;
 
     FtcDashboard dashboard = FtcDashboard.getInstance();
     TelemetryPacket packet = new TelemetryPacket();
-    DcMotorEx mtr = hardwareMap.get(DcMotorEx.class, "mtr");
+    DcMotorEx mtr;
 
     @Override
     public void init() {
-        mtr.setVelocityPIDFCoefficients(DRIVE_PID.p, DRIVE_PID.i, DRIVE_PID.d, DRIVE_PID.f);
-        mtr.setTargetPositionTolerance(TOLERANCE);  // 허용오차 설정
+        mtr = hardwareMap.get(DcMotorEx.class, "mtr");
+        mtrInit();
     }
 
     @Override
@@ -30,10 +36,15 @@ public class Driving extends OpMode {
             move();
         }
         else if (gamepad1.y) {
-            mtr.setVelocityPIDFCoefficients(DRIVE_PID.p, DRIVE_PID.i, DRIVE_PID.d, DRIVE_PID.f);
+            mtrInit();
         }
 
         showData();
+    }
+
+    private void mtrInit() {
+        mtr.setVelocityPIDFCoefficients(DRIVE_PID.p, DRIVE_PID.i, DRIVE_PID.d, DRIVE_PID.f);
+        mtr.setTargetPositionTolerance(TOLERANCE);  // 허용오차 설정
     }
 
     /**
@@ -46,12 +57,12 @@ public class Driving extends OpMode {
     private void move() {
         mtr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        mtr.setTargetPosition(45);
+        mtr.setTargetPosition(TARGET_POS);
 
         mtr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         // Velocity의 PIDF계수 사용X -> TargetPos의 P계수만 사용. 즉 TargetPos랑 Velocity를 같이 사용하면, Velocity는 최대속도 제한용 인거임
         // 근데 짜피 PinPoint사용하면 바퀴 굴릴땐 targetPosition못씀. 로봇팔에선 쓸 수 있겠네
-        mtr.setVelocity(DriveConfig.TPS);
+        mtr.setVelocity(TPS);
     }
 
     private void showData() {
