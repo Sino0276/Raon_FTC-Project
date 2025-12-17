2e6e56987110bfa3115269acb19dbcbbe049a83b
sineno <106243747+Sino0276@users.noreply.github.com>
2025-12-17 14:51:20 +0900

    [add] PedroPathing and FtcLib

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TurretTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.tests;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
